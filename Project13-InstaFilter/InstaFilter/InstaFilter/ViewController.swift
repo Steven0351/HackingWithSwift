e4bf5a504b1dd3072f57303cb439d4d1f9aa6aa1
Steven0351 <steven.sherry@affinityforapps.com>
2017-06-30 06:09:12 -0500

    Started Project 13

---
+++ Project13-InstaFilter/InstaFilter/InstaFilter/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  InstaFilter
+//
+//  Created by Steven Sherry on 6/30/17.
+//  Copyright © 2017 Steven Sherry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self, action: #selector(importPicture))
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+    func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+}
+
