3c6d57d6bd3340aa68c70f9a725e5daf7239e590
Steven0351 <steven.sherry@affinityforapps.com>
2017-07-10 06:09:05 -0500

    figured out positions and speeds for targets

--- Milestone8Challenge/ShootingGallery/ShootingGallery/GameScene.swift
+++ Milestone8Challenge/ShootingGallery/ShootingGallery/GameScene.swift
@@ -14,9 +14,15 @@ class GameScene: SKScene {
     var bullets = [SKSpriteNode]()
     var bulletsRemaining: Int = 6 {
         didSet {
-            // change bullets
+            guard let bullet = bullets.popLast() else { return }
+            bullet.removeFromParent()
+            if bulletsRemaining <= 0 {
+                reloadLabel.fontColor = UIColor.white
+                return
+            }
         }
     }
+    var reloadLabel: SKLabelNode!
     var scoreLabel: SKLabelNode!
     var score: Int = 0 {
         didSet {
@@ -38,6 +44,13 @@ class GameScene: SKScene {
         scoreLabel.zPosition = 0
         addChild(scoreLabel)
         
+        reloadLabel = SKLabelNode(fontNamed: "Chalkduster")
+        reloadLabel.text = "Reload"
+        reloadLabel.position = CGPoint(x: 512, y: 725)
+        reloadLabel.zPosition = 0
+        reloadLabel.fontColor = UIColor.clear
+        addChild(reloadLabel)
+        
         for multiplier in 0 ..< bulletsRemaining {
             let bullet = SKSpriteNode(imageNamed: "bullet")
             bullet.position = CGPoint(x: 50 + CGFloat(multiplier * 10),
@@ -47,15 +60,28 @@ class GameScene: SKScene {
             addChild(bullet)
         }
         
+        createTargets(perRow: 0)
+        
     }
     
     
     
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let bullet = bullets.popLast() else { return }
-        bullet.removeFromParent()
         
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let nodesAtPoint = nodes(at: location)
+        
+        
+        for node in nodesAtPoint {
+            if node.isEqual(to: reloadLabel) {
+                reload()
+                return
+            } else if bulletsRemaining > 0 {
+                bulletsRemaining -= 1
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,5 +92,86 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func createTargets(perRow targets: Int) {
+        print(targets)
+        let topAndBottomStart = CGFloat(-55)
+        let middleStart = CGFloat(1084)
+        
+        let bottomRow = CGFloat(100)
+        let middleRow = CGFloat(340)
+        let topRow = CGFloat(590)
+        
+        let bigSpeedInSeconds = 8.0
+        let mediumSpeedInseconds = 5.0
+        let smallSpeedInSeconds = 2.0
+        
+        
+        
+        var goodTarget: SKSpriteNode!
+        var badTarget: SKSpriteNode!
+        var otherTarget: SKSpriteNode!
+        
+//        let goodRandomInt = GKRandomSource.sharedRandom().nextInt(upperBound: 2)
+//        let badRandomInt = GKRandomSource.sharedRandom().nextInt(upperBound: 2)
+//        
+//        switch goodRandomInt {
+//        case 0:
+//            goodTarget = SKSpriteNode(imageNamed: "goodTargetBig")
+//            goodTarget.name = "goodBig"
+//        case 1:
+//            goodTarget = SKSpriteNode(imageNamed: "goodTargetMedium")
+//            goodTarget.name = "goodMedium"
+//        case 2:
+//            goodTarget = SKSpriteNode(imageNamed: "goodTargetSmall")
+//            goodTarget.name = "goodSmall"
+//        default:
+//            return
+//        }
+//        
+//        switch badRandomInt {
+//        case 0:
+//            badTarget = SKSpriteNode(imageNamed: "badTargetBig")
+//            badTarget.name = "bad"
+//        case 1:
+//            badTarget = SKSpriteNode(imageNamed: "badTargetMedium")
+//            badTarget.name = "bad"
+//        case 2:
+//            badTarget = SKSpriteNode(imageNamed: "badTargetSmall")
+//            badTarget.name = "bad"
+//        default:
+//            return
+//        }
+        
+        goodTarget = SKSpriteNode(imageNamed: "goodTargetBig")
+        badTarget = SKSpriteNode(imageNamed: "badTargetBig")
+        otherTarget = SKSpriteNode(imageNamed: "goodTargetBig")
+        
+        goodTarget.position = CGPoint(x: topAndBottomStart, y: bottomRow)
+        badTarget.position = CGPoint(x: topAndBottomStart, y: topRow)
+        otherTarget.position = CGPoint(x: middleStart, y: middleRow)
+        
+        addChild(goodTarget)
+        addChild(badTarget)
+        addChild(otherTarget)
+        
+        goodTarget.run(SKAction.move(to: CGPoint(x: middleStart, y: goodTarget.position.y), duration: bigSpeedInSeconds))
+        badTarget.run(SKAction.move(to: CGPoint(x: middleStart, y: badTarget.position.y), duration: mediumSpeedInseconds))
+        otherTarget.run(SKAction.move(to: CGPoint(x: topAndBottomStart, y: middleRow), duration: smallSpeedInSeconds))
+        
+    }
+    
+    func reload() {
+        bulletsRemaining = 0
+        let maxBullets = 6
+        for multiplier in bulletsRemaining ... maxBullets {
+            let bullet = SKSpriteNode(imageNamed: "bullet")
+            bullet.position = CGPoint(x: 50 + CGFloat(multiplier * 10), y: 725)
+            bullet.zPosition = 0
+            bullets.append(bullet)
+            addChild(bullet)
+            reloadLabel.fontColor = UIColor.clear
+        }
     }
 }
