5bbf54b5e2a42d7c2efdaef779b03fdb86ad8cbc
Steven0351 <steven.sherry@affinityforapps.com>
2017-07-09 20:03:45 -0500

    Created background and bullets and added to scene. Enabled removed bullets from scene on touch

--- Milestone8Challenge/ShootingGallery/ShootingGallery/GameScene.swift
+++ Milestone8Challenge/ShootingGallery/ShootingGallery/GameScene.swift
@@ -10,16 +10,51 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    var bullets = [SKSpriteNode]()
+    var bulletsRemaining: Int = 6 {
+        didSet {
+            // change bullets
+        }
+    }
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+           scoreLabel.text = "Score: \(score)"
+        }
+    }
 
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.zPosition = -1
+        background.blendMode = .replace
+        addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.position = CGPoint(x: 900, y: 725)
+        scoreLabel.zPosition = 0
+        addChild(scoreLabel)
+        
+        for multiplier in 0 ..< bulletsRemaining {
+            let bullet = SKSpriteNode(imageNamed: "bullet")
+            bullet.position = CGPoint(x: 50 + CGFloat(multiplier * 10),
+                                      y: 725)
+            bullet.zPosition = 0
+            bullets.append(bullet)
+            addChild(bullet)
+        }
         
     }
     
     
+    
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        guard let bullet = bullets.popLast() else { return }
+        bullet.removeFromParent()
         
     }
     
