1417ed063ffe7dd82c40dff539581c79d6e5bc4f
Steven0351 <steven.sherry@affinityforapps.com>
2017-07-15 20:29:18 -0500

    completed milestone 10 challenge

---
+++ Milestone10Challenge/Graphics/Graphics/ViewController.swift
@@ -0,0 +1,199 @@
+//
+//  ViewController.swift
+//  Graphics
+//
+//  Created by Steven Sherry on 7/15/17.
+//  Copyright © 2017 Steven Sherry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        drawRectangle()
+    }
+    
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = img
+    }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = img
+    }
+
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        imageView.image = img
+    }
+    
+    func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            let rotations = 32
+            let amount = Double.pi / Double(rotations)
+            
+            for _ in 0 ..< rotations {
+                ctx.cgContext.rotate(by: CGFloat(amount))
+                ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        imageView.image = img
+    }
+    
+    func drawLines() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0 ..< 256 {
+                ctx.cgContext.rotate(by: CGFloat.pi / 2)
+                
+                if first {
+                    ctx.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    ctx.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                length *= 0.99
+            }
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        imageView.image = img
+    }
+    
+    func drawImagesAndText() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs = [NSFontAttributeName: UIFont(name: "HelveticaNeue-Thin", size: 36)!,
+                         NSParagraphStyleAttributeName: paragraphStyle]
+            
+            let string = "The best-laid schemes o'\nmice and' men gang aft agley"
+            string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin,
+                        attributes: attrs, context: nil)
+
+            let mouse = UIImage(named: "mouse")
+            mouse?.draw(at: CGPoint(x: 300, y: 150))
+        }
+        imageView.image = img
+
+    }
+    
+    func drawRainbow() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let center = CGPoint(x: imageView.bounds.width/3,
+                             y: imageView.bounds.height/3)
+        let img = renderer.image { ctx in
+            // My solution - in four less lines of code if I might add!
+            let colors = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor,
+                          UIColor.blue.cgColor, UIColor.purple.cgColor, UIColor.magenta.cgColor, UIColor.white.cgColor]
+            let radius = 250
+            for (index, color) in colors.enumerated() {
+                ctx.cgContext.addArc(center: center, radius: CGFloat(radius - (index * 15)), startAngle: 0,
+                                     endAngle: CGFloat.pi, clockwise: true)
+                ctx.cgContext.setFillColor(color)
+                ctx.cgContext.drawPath(using: .fill)
+            }
+//            Code from book:
+//            
+//            UIColor.white.set()
+//            ctx.fill(CGRect(x: 0, y: 0, width: 512, height: 512))
+//            ctx.cgContext.translateBy(x: 0, y: 256)
+//            let colors = [UIColor.red, UIColor.orange, UIColor.yellow,
+//                          UIColor.green, UIColor.blue, UIColor.purple, UIColor.white]
+//            var drawRect = CGRect(x: 5, y: 5, width: 502, height: 502)
+//            for color in colors {
+//                drawRect = drawRect.insetBy(dx: 20, dy: 20)
+//                ctx.cgContext.setFillColor(color.cgColor)
+//                ctx.cgContext.addEllipse(in: drawRect)
+//                ctx.cgContext.drawPath(using: .fill)
+//            }
+
+        }
+        imageView.image = img
+    }
+
+
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 6 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
+        case 3:
+            drawRotatedSquares()
+        case 4:
+            drawLines()
+        case 5:
+            drawImagesAndText()
+        case 6:
+            drawRainbow()
+        default:
+            break
+        }
+    }
+
+}
+
