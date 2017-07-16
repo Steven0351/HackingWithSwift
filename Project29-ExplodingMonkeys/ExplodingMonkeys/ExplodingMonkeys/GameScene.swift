fd543c245f89581e38412efe7b736c2d858c8f31
Steven0351 <steven.sherry@affinityforapps.com>
2017-07-16 03:39:51 -0500

    Started on project 29

---
+++ Project29-ExplodingMonkeys/ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
@@ -0,0 +1,52 @@
+//
+//  GameScene.swift
+//  ExplodingMonkeys
+//
+//  Created by Steven Sherry on 7/16/17.
+//  Copyright © 2017 Steven Sherry. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
+class GameScene: SKScene {
+    
+    var buildings = [BuildingNode]()
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(min: 2, max: 4) * 40, height: RandomInt(min: 300, max: 600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
