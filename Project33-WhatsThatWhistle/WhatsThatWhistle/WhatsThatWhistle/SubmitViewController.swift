1ee71fbb02edc5fc9ac41de24e4250de9c5ddbcf
Steven0351 <steven.sherry@affinityforapps.com>
2017-07-20 07:54:47 -0500

    Got cloud kit uploading working

--- Project33-WhatsThatWhistle/WhatsThatWhistle/WhatsThatWhistle/SubmitViewController.swift
+++ Project33-WhatsThatWhistle/WhatsThatWhistle/WhatsThatWhistle/SubmitViewController.swift
@@ -66,7 +66,30 @@ class SubmitViewController: UIViewController {
     }
     
     func doSubmission() {
+        let whistleRecord = CKRecord(recordType: "Whistles")
+        whistleRecord["genre"] = genre as CKRecordValue
+        whistleRecord["comments"] = comments as CKRecordValue
         
+        let audioURL = RecordWhistleViewController.getWhistleURL()
+        let whistleAsset = CKAsset(fileURL: audioURL)
+        whistleRecord["audio"] = whistleAsset
+        
+        CKContainer.default().publicCloudDatabase.save(whistleRecord) { [unowned self] record, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.status.text = "Error: \(error.localizedDescription)"
+                    self.spinner.stopAnimating()
+                } else {
+                    self.view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+                    self.status.text = "Done!"
+                    self.spinner.stopAnimating()
+                    ViewController.isDirty = true
+                    
+                }
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self,
+                                                                         action: #selector(self.doneTapped))
+            }
+        }
     }
     
     func doneTapped () {
