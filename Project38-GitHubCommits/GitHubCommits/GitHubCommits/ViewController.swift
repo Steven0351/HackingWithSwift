789bc894f61417686a401c2930f05378226c7448
Steven0351 <steven.sherry@affinityforapps.com>
2017-07-22 11:58:21 -0500

    started project 38

---
+++ Project38-GitHubCommits/GitHubCommits/GitHubCommits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubCommits
+//
+//  Created by Steven Sherry on 7/22/17.
+//  Copyright © 2017 Steven Sherry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
